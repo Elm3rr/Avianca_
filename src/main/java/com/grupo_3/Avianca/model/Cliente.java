e111095206808075ac391866466a2bdb43269557
nagi-hash18 <tzuljose0818@gmail.com>
2024-08-30 21:38:23 -0600

    prueba

---
+++ src/main/java/com/grupo_3/Avianca/model/Cliente.java
@@ -0,0 +1,82 @@
+package com.grupo_3.Avianca.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+    @Id
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String correo;
+    private String telefono;
+    private String DPI;
+    private String NIT;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDPI() {
+        return DPI;
+    }
+
+    public void setDPI(String dPI) {
+        DPI = dPI;
+    }
+
+    public String getNIT() {
+        return NIT;
+    }
+
+    public void setNIT(String nIT) {
+        NIT = nIT;
+    }
+
+}
