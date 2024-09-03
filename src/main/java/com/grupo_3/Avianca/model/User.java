8b7410582df72185e9a206817cabafaccf49bdb7
Elm3rr <elmerraul2004@gmail.com>
2024-09-02 20:43:32 -0600

    hola

---
+++ src/main/java/com/grupo_3/Avianca/model/User.java
@@ -0,0 +1,24 @@
+package com.grupo_3.Avianca.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String username;
+    private String password;
+
+}
